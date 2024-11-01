81d8227728a526915ade5fb2838932d350f1cd85
Фролов Александр <frolov.mailbox@mail.ru>
2024-11-01 13:36:39 +0300

    Task001 is made

--- HW002/Task001.py
+++ HW002/Task001.py
@@ -1,1 +1,11 @@
-print("Hello, world")
+rating = int(input("Enter the num: "))
+positive_count = 0
+negative_count = 0
+while rating !=0:
+    if rating > 0:
+        positive_count +=1
+    else:
+        negative_count +=1
+rating = int(input("Enter the num: "))
+print ("The ammount of positive nums: ", positive_count)   
+print ("The amount of negative nums: ", negative_count)     
