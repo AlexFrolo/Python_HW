e4ecfe13fc4d4a87d59c9871534708cd5cb57894
Фролов Александр <frolov.mailbox@mail.ru>
2024-11-15 14:21:06 +0300

    saved

--- HW005/Task003.py
+++ HW005/Task003.py
@@ -7,6 +7,5 @@ def my_sum(*args):
             for x in i_elem:
                 total_sum += my_sum(x)
         return total_sum
-# Основной код для тестирования
-print(my_sum([[1, 2, [3]], [1], 3])) # Ожидаемый результат: 10
-print(my_sum(1, 2, 3, 4, 5)) # Ожидаемый результат: 15
+print(my_sum([[1, 2, [3]], [1], 3])) 
+print(my_sum(1, 2, 3, 4, 5)) 
